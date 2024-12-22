73c54b1a4a1b4471e92dc9a2689b9ec8f8c0e44a
rafsanhasankhan <rafsanhasankhaninfo@gmail.com>
2024-12-22 08:00:00 +0600

    sort and search query complete.

--- client/src/pages/Products/Products/Products.jsx
+++ client/src/pages/Products/Products/Products.jsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from 'react';
-
+import { FaFilter } from "react-icons/fa";
 const Products = () => {
   const [products, setProducts] = useState([]);
   const [filteredItems, setFilteredItems] = useState([]);
   const [searchQuery, setSearchQuery] = useState('');
+  const [suggestions, setSuggestions] = useState([]);
+  const [sortOption, setSortOption] = useState('default');
 
   useEffect(() => {
     const fetchData = async () => {
@@ -20,23 +22,62 @@ const Products = () => {
   }, []);
 
   // filter based category
-  const filterItems = (category) => {
-    const filtered = category === 'all' ? products : products.filter(item => item.category === category)
-    setFilteredItems(filtered)
-  }
+  const filterItems = category => {
+    const filtered =
+      category === 'all'
+        ? products
+        : products.filter(item => item.category === category);
+    setFilteredItems(filtered);
+  };
   const showAll = () => {
-    setFilteredItems(products)
-  }
+    setFilteredItems(products);
+  };
 
   // Search & Suggestions
-  useEffect(()=>{
-    if(searchQuery === '') {
-      setFilteredItems(products)
+  useEffect(() => {
+    if (searchQuery === '') {
+      setSuggestions([]);
+      setFilteredItems(products);
     } else {
-      const filtered = products.filter(item => item.name.toLowerCase().includes(searchQuery.toLocaleLowerCase()) )
-      setFilteredItems(filtered)
+      const matchingSuggestions = products.filter(item =>
+        item.name.toLowerCase().includes(searchQuery.toLocaleLowerCase())
+      );
+      setSuggestions(matchingSuggestions.slice(0, 5));
+      setFilteredItems(matchingSuggestions);
     }
-  },[products, searchQuery])
+  }, [products, searchQuery]);
+
+  // Sorting
+  const handleSortChange = option => {
+    setSortOption(option);
+    let sortedItems = [...filteredItems];
+    // logic
+    switch (option) {
+      case 'A-Z':
+        sortedItems.sort((a, b) => a.name.localeCompare(b.name));
+        break;
+      case 'Z-A':
+        sortedItems.sort((a, b) => b.name.localeCompare(a.name));
+        break;
+      case 'low-to-high':
+        sortedItems.sort(
+          (a, b) =>
+            parseFloat(a.price.replace('$', '')) -
+            parseFloat(b.price.replace('$', ''))
+        );
+        break;
+      case 'high-to-low':
+        sortedItems.sort(
+          (a, b) =>
+            parseFloat(b.price.replace('$', '')) -
+            parseFloat(a.price.replace('$', ''))
+        );
+        break;
+      default: ;
+        break;
+    }
+    setFilteredItems(sortedItems);
+  };
 
   return (
     <section className="section-container px-4 lg:px-0 py-32">
@@ -59,26 +100,63 @@ const Products = () => {
       </div>
       {/* </div> */}
       <div className="my-24">
-        <div className='flex justify-between items-start'>
+        <div className="relative flex justify-between items-start">
           <h2 className="bebas-neue-regular text-5xl">All Products</h2>
           <input
-            onChange={(e)=>setSearchQuery(e.target.value)}
+            onChange={e => setSearchQuery(e.target.value)}
             type="text"
-            placeholder="Type here"
+            placeholder="Search here"
             className="input input-bordered input-ghost w-full max-w-xs"
           />
+          {suggestions.length > 0 && (
+            <ul className="absolute right-0 top-14 w-[319px] bg-white border border-gray-300  rounded shadow-md z-10">
+              {suggestions.map((suggestion, index) => (
+                <li
+                  key={index}
+                  onClick={() => suggestion.name}
+                  className="px-4 py-2 cursor-pointer hover:bg-gray-200"
+                >
+                  {suggestion.name}
+                </li>
+              ))}
+            </ul>
+          )}
         </div>
         {/* category & filter */}
-        <div className='flex justify-between items-center mt-10 mb-4'>
+        <div className="flex justify-between items-center mt-10 mb-4">
           {/* all category */}
-          <div className='space-x-1 font-medium'>
-            <button onClick={showAll} className='rounded px-2 py-1 btn-ghost'>All</button>
-            <button onClick={()=>filterItems('men')} className='rounded px-2 py-1 btn-ghost'>Men</button>
-            <button onClick={()=>filterItems('women')} className='rounded px-2 py-1 btn-ghost'>Woman</button>
+          <div className="space-x-1 font-medium">
+            <button onClick={showAll} className="rounded px-2 py-1 btn-ghost">
+              All
+            </button>
+            <button
+              onClick={() => filterItems('men')}
+              className="rounded px-2 py-1 btn-ghost"
+            >
+              Men
+            </button>
+            <button
+              onClick={() => filterItems('women')}
+              className="rounded px-2 py-1 btn-ghost"
+            >
+              Woman
+            </button>
           </div>
           {/* filter */}
-          <div>
-            <button>filter</button>
+          <div className='flex justify-center items-center gap-1'>
+          <FaFilter className=' bg-black text-white p-2 rounded text-[37px]'></FaFilter>
+            <select
+              
+              onChange={e => handleSortChange(e.target.value)}
+              value={sortOption}
+              className="bg-black text-white px-2 py-2 rounded cursor-pointer"
+            >
+              <option value="default">Default</option>
+              <option value="A-Z">A-Z</option>
+              <option value="Z-A">Z-A</option>
+              <option value="low-to-high">Price: Low To High</option>
+              <option value="high-to-low">Price: High To Low</option>
+            </select>
           </div>
         </div>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 items-center gap-8">
@@ -93,9 +171,10 @@ const Products = () => {
               <div className="card-body">
                 <h2 className="card-title">{product.name}</h2>
                 <p>{product.description}</p>
+                <span>{product.price}</span>
                 <div className="card-actions justify-end">
                   <button className="btn bg-gradient-to-b from-[#A4BC46] from-[0%] to-[#85A019] to-[100%] text-white">
-                    Buy Now
+                    Add To Cart
                   </button>
                 </div>
               </div>
